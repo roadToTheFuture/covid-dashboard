c90569bf664ee0c33b5d79ed8ce57678053a2663
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2020-12-19 23:49:09 +0300

    feat: add new folders

---
+++ src/js/events/events.js
@@ -0,0 +1,2 @@
+import '@js/elements/events/click/click.js';
+import '@js/elements/events/request/request.js';
--- src/js/utils/getDataAPI.js
+++
@@ -1,7 +0,0 @@
-fetch('https://api.covid19api.com/summary')
-.then((response) => {
-  return response.json();
-})
-.then((data) => {
-console.log(data);
-});
