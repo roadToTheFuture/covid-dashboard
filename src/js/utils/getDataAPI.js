b7c7c09ae338126ad401c0d7e8eb2e55721b7765
Mykola <nazimkoff@gmail.com>
2020-12-20 09:36:54 +0200

    feat: add fetch

--- src/js/utils/getDataAPI.js
+++ src/js/utils/getDataAPI.js
@@ -1,7 +1,10 @@
-fetch('https://api.covid19api.com/summary')
+const BASE_URL = 'https://api.covid19api.com';
+fetch(`${BASE_URL}/summary`)
 .then((response) => {
   return response.json();
 })
 .then((data) => {
 console.log(data);
+return data;
 });
+
