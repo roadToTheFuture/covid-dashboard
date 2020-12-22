84222f7acc9f4581c02ed5a5dca6c579e5e1f22d
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2020-12-22 20:13:35 +0300

    feat: add graph

--- src/js/elements/main/global/graph.js
+++ src/js/elements/main/global/graph.js
@@ -1,6 +1,7 @@
 import { createDomElement } from '@js/utils/createDomElement.js';
 import { global } from '@js/elements/main/slider/slider.js';
 
-const globalGraph = createDomElement('canvas', 'graph', '', global);
+const globalGraph = createDomElement('div', 'graph', '', global);
+const globalCanvas = createDomElement('canvas', '', '', globalGraph);
 
-export { globalGraph };
+export { globalGraph, globalCanvas };
--- src/js/events/global/firstReload.js
+++ src/js/events/global/firstReload.js
@@ -1,7 +1,9 @@
 import generateCases from '@js/events/global/click/cases.js';
+import renderGlobalGraph from '@js/utils/renderGlobalGraph.js';
 import { DOM_GLOBAL_ELEMENT } from '@js/constants/constants.js';
 
 generateCases();
+renderGlobalGraph();
 
 const day = new Date().getDate();
 const month = new Date().getMonth();
