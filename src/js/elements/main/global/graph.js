b713da2e277acb8bfcf4bb8d7622a300772f59ab
roadToTheFuture <63185603+roadToTheFuture@users.noreply.github.com>
2020-12-22 18:05:28 +0300

    feat: add canvas

--- src/js/elements/main/global/graph.js
+++ src/js/elements/main/global/graph.js
@@ -1,6 +1,6 @@
 import { createDomElement } from '@js/utils/createDomElement.js';
 import { global } from '@js/elements/main/slider/slider.js';
 
-const globalGraph = createDomElement('div', 'graph', '', global);
+const globalGraph = createDomElement('canvas', 'graph', '', global);
 
 export { globalGraph };
